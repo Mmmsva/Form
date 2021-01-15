07f508fc7172fe3b55f1cc37a6c4d05735598c50
Mmmsva <mmmdvsaa@gmail.com>
2021-01-15 09:52:48 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import Validation from "./UserValidation";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
+        style={{
+          fontFamily: "monospace",
+          fontSize: 40,
+          color: "coral",
+        }}
+      >
+        FORM
+      </h1>
+
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          birthDate: "",
+          age: "",
+        }}
+        validationSchema={Validation}
+        onSubmit={(fields) => {
+          console.log(fields);
+        }}
+      >
+        <Form style={{ display: "flex", flexDirection: "column" }}>
+          <Field name="firstName" type="text" placeholder="First Name" />
+          <ErrorMessage
+            name="firstName"
+            component="div"
+            className="invalid-feedback"
+            style={{ color: "red", fontWeight: "bold" }}
+          />
+
+          <Field name="lastName" type="text" placeholder="Last Name" />
+          <ErrorMessage
+            name="lastName"
+            component="div"
+            className="invalid-feedback"
+            style={{ color: "red", fontWeight: "bold" }}
+          />
+
+          <Field name="email" type="email" placeholder="Email" />
+          <ErrorMessage
+            name="email"
+            component="div"
+            className="invalid-feedback"
+            style={{ color: "red", fontWeight: "bold" }}
+          />
+
+          <Field name="password" type="text" placeholder="Password" />
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="invalid-feedback"
+            style={{ color: "red", fontWeight: "bold" }}
+          />
+
+          <Field name="birthDate" type="date" placeholder="Birth Date" />
+          <ErrorMessage
+            name="birthDate"
+            component="div"
+            className="invalid-feedback"
+            style={{ color: "red", fontWeight: "bold" }}
+          />
+
+          <Field name="age" type="number" placeholder="Age" />
+          <ErrorMessage
+            name="age"
+            component="div"
+            className="invalid-feedback"
+            style={{ color: "red", fontWeight: "bold" }}
+          />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
